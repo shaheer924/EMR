f079164d08c005df2c3f5613c203480362b306cc
shaheer924 <39200747+shaheer924@users.noreply.github.com>
2023-02-18 19:16:40 +0500

    Delete src directory

--- src/Server.ts
+++
@@ -1,9 +0,0 @@
-import App from './App'
-import dotenv from 'dotenv'
-
-dotenv.config({path: '.env'})
-
-// @ts-ignore
-const app = new App(process.env.PORT, process.env.MONGODB_URI)
-
-app.listen()
